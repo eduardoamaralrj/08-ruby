ddfaee06d4a7db2a1f4eeda334c65b6df707bb13
Eduardo Amaral <76413887+eduardoamaralrj@users.noreply.github.com>
2024-09-15 19:38:18 -0300

    Add files via upload

---
+++ alg.rb
@@ -0,0 +1,16 @@
+nome = "João"
+idade = 25
+altura = 1.75
+
+puts "Nome: #{nome}"
+puts "Idade: #{idade} anos"
+puts "Altura: #{altura} metros"
+
+
+nome2 = "Pedro"
+idade2 = 30
+altura2 = 1.80
+
+puts "Nome: #{nome2}"
+puts "Idade: #{idade2} anos"
+puts "Altura: #{altura2} metros"
